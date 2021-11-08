3f46892f3465278c4b9629dff221178be6b1d053
maihuytech@gmail.com <maihuytech@gmail.com>
2021-11-08 11:13:51 +0700

    init

---
+++ src/main/java/com/example/redis_crudmph/RedisCrudmphApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis_crudmph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCrudmphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCrudmphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redis_crudmph/RedisCrudmphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis_crudmph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCrudmphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
